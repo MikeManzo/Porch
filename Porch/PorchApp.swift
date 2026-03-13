604ae0e41a161e5319ddca2cdaf7b68dc8fc5e59
Mike Manzo <mike.manzo@.com>
2026-03-13 17:47:42 -0400

    Initial Commit

---
+++ Porch/PorchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PorchApp.swift
+//  Porch
+//
+//  Created by Mike Manzo on 3/13/26.
+//
+
+import SwiftUI
+
+@main
+struct PorchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
